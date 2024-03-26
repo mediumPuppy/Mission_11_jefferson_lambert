46e2c877779f95655adb2461b24bfd9b2631b566
mediumpuppy <83100592+mediumPuppy@users.noreply.github.com>
2024-03-25 23:05:51 -0600

    components begin

---
+++ Mission_11_jefferson_lambert/Components/ProjectTypesViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Mission_11_jefferson_lambert.Components
+{
+    public class ProjectTypesViewComponent : ViewComponent
+    {
+        public string Invoke()
+        {
+            return "ThisWorks teehee";
+        }
+    }
+}
