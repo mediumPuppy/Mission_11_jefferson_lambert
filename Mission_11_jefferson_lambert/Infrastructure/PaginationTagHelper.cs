68eee127615e38fb59c8345457b29985fe3319a1
mediumpuppy <83100592+mediumPuppy@users.noreply.github.com>
2024-03-25 19:00:55 -0600

    styling update, url update

--- Mission_11_jefferson_lambert/Infrastructure/PaginationTagHelper.cs
+++ Mission_11_jefferson_lambert/Infrastructure/PaginationTagHelper.cs
@@ -22,6 +22,10 @@ namespace Mission_11_jefferson_lambert.Infrastructure
         public ViewContext? ViewContext { get; set; }
         public string? PageAction { get; set; }
         public PaginationInfo PageModel { get; set; }
+        public bool PageClassesEnabled { get; set; } = false;
+        public string PageClass { get; set; } = String.Empty;
+        public string PageClassNormal { get; set; } = String.Empty;
+        public string PageClassSelected { get; set; } = String.Empty;
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
             if (ViewContext != null && PageModel != null)
@@ -32,6 +36,11 @@ namespace Mission_11_jefferson_lambert.Infrastructure
                 {
                     TagBuilder tag = new TagBuilder("a");
                     tag.Attributes["href"] = urlHelper.Action(PageAction, new { pageNum = i });
+                    if (PageClassesEnabled)
+                    {
+                        tag.AddCssClass(PageClass);
+                        tag.AddCssClass (i==PageModel.CurrentPage ? PageClassSelected : PageClassNormal);
+                    }
                     tag.InnerHtml.Append(i.ToString() + "\t");
                     result.InnerHtml.AppendHtml(tag);
 
